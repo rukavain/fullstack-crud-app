a83fb8f08297847e5fc528bc018a7f4edd373731
rukavain1~ <magtoto599@gmail.com>
2024-01-23 21:05:59 +0800

    added protected fillable

--- app/Models/Bread.php
+++ app/Models/Bread.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Bread extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'price',
+        'stocks'
+    ];
 }
